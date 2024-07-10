82cb7eb0ddb2757dee45998d43effb62b916edb4
kavyakelam <kelamkavyakumar@gmail.com>
2024-07-10 19:57:00 +0530

    Update rockpaperscissor.py

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -4,16 +4,16 @@ while True:
    user = input("what's ur choice? 'r' --> rock,'p' -->  paper,'s' --> scissor\n")
    computer= random.choice(['r','p','s'])
    if user == computer: 
-     print("its a tie")
+     print("It's a Tie")
    elif user=='r' and computer=='p':
-     print("you lose!")
+     print("you lose")
    elif user =='r' and computer=='s':
-     print("you win!") 
+     print("you win") 
    elif user =='p' and computer=='s':
-     print("you lose!")  
+     print("you lose")  
    else :
-     print("please try again!")
+     print("Please try again!")
    play_again = input("play again? (y/n):")  
    if not play_again == 'y':
-       print ("thanks for playing")
+       print ("Thanks for Playing")
        break
